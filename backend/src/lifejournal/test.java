d715850a6f9facd663ae0040bb930e36161c0282
shalin94 <shalin94@gmail.com>
2014-09-27 20:54:57 -0700

    Deploy to heroku

---
+++ backend/src/lifejournal/test.java
@@ -0,0 +1,21 @@
+package lifejournal;
+
+import org.parse4j.*;
+
+public class test {
+	static String APP_ID = "Tm6o3fOxEyVu7Cl6DbumfVrGT4q8J8QjXPo8N3tC";
+	static String APP_REST_API_ID = "0eyn7DE1Ypo7iRQgMcE3gUGRfjLxTs6Z9UYxMJJP";
+	public static void testFunc() {
+		Parse.initialize(APP_ID, APP_REST_API_ID);
+		ParseObject userProfile = new ParseObject("UserProfile");
+		userProfile.put("username", "shalin94@gmail.com");
+		userProfile.put("name", "Shalin Shah");
+		userProfile.put("password", "test");
+		try {
+			userProfile.save();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
---
+++ backend/src/testServlet.java
@@ -0,0 +1,44 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import lifejournal.test;
+
+/**
+ * Servlet implementation class testServlet
+ */
+@WebServlet("/testServlet")
+public class testServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public testServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		test test1 = new test();
+		test1.testFunc();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
